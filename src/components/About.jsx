fee759d0f148b3c2c15325121b83178becfd7d49
Malinda Prabath <malindaprabath876@gmail.com>
2025-03-27 19:00:26 +0530

    Start develop about section

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      className="container flex flex-col items-center justify-center w-full mx-auto overflow-hidden p-14 md:px-32 lg:px-20"
+      id="About"
+    >
+      <h1>
+        <span>Our Brand</span>
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, in
+        tempora.
+      </p>
+    </div>
+  );
+};
+
+export default About;
