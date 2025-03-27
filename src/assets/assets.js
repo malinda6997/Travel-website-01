1c2bc38f22610b599cef94fc063595a5e46271a8
Malinda Prabath <malindaprabath876@gmail.com>
2025-03-27 13:10:21 +0530

    fixed header_img errors

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,6 @@ import menu_icon from './menu_icon.svg'
 import star_icon from './star_icon.svg'
 import left_arrow from './left_arrow.svg'
 import right_arrow from './right_arrow.svg'
-import header_img from './header_img.png'
 import brand_img from './brand_img.png'
 import project_img_1 from './project_img_1.jpg'
 import project_img_2 from './project_img_2.jpg'
@@ -17,13 +16,13 @@ import profile_img_1 from './profile_img_1.png'
 import profile_img_2 from './profile_img_2.png'
 import profile_img_3 from './profile_img_3.png'
 
+
 export const assets = {
     logo,
     logo_dark,
     cross_icon,
     menu_icon,
     star_icon,
-    header_img,
     brand_img,
     project_img_1,
     project_img_2,
@@ -31,6 +30,7 @@ export const assets = {
     project_img_4,
     left_arrow,
     right_arrow,
+
 }
 
 export const projectsData = [
